14148f2d9450b08db1ea1b141b5abaf8e4b13142
409 <409@academicos.cesde.edu.co>
2022-10-03 14:15:10 -0500

    creaciones peticiones

---
+++ src/main/java/com/tcc/api/controladores/MercanciaControlador.java
@@ -0,0 +1,13 @@
+package com.tcc.api.controladores;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping ("api/tcc/mercancias")
+public class MercanciaControlador {
+
+
+
+}
